7c258e150a471b0c2e0127507696458bbac93d59
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-12-09 13:46:43 -0300

    feat: add missing param error to customers domain

--- booking_service/domain/customers/entities.py
+++ booking_service/domain/customers/entities.py
@@ -1,5 +1,9 @@
 from datetime import datetime
-from .exceptions import InvalidCustomerDocumentException, CustomerShouldBeOlderThan18
+from .exceptions import (
+    InvalidCustomerDocumentException,
+    CustomerShouldBeOlderThan18,
+    MissingParamError,
+)
 
 
 class Customer:
@@ -7,19 +11,33 @@ class Customer:
     birth_date: datetime
     document: str
     email: str
+    id: str | None
 
     def __init__(
-        self, name: str, birth_date: datetime, document: str, email: str
+        self,
+        name: str,
+        birth_date: datetime,
+        document: str,
+        email: str,
+        id: str | None = None,
     ) -> None:
         self.name = name
         self.birth_date = birth_date
         self.document = document
         self.email = email
+        self.id = id
+
+    def create_customer(self):
+        self.is_valid()
 
     def is_valid(self):
+        required_fields = ["name", "birth_date", "document", "email"]
+        for field in required_fields:
+            if getattr(self, field) is None:
+                raise MissingParamError(field)
         if len(self.document) < 5:
             raise InvalidCustomerDocumentException("Invalid document number")
-        elif (
+        if (
             datetime.today().year
             - self.birth_date.year
             - (
@@ -29,5 +47,4 @@ class Customer:
             < 18
         ):
             raise CustomerShouldBeOlderThan18("Customer should be older than 18")
-
         return True
--- booking_service/domain/customers/exceptions.py
+++ booking_service/domain/customers/exceptions.py
@@ -6,3 +6,8 @@ class CustomerShouldBeOlderThan18(Exception):
 class InvalidCustomerDocumentException(Exception):
     def __init__(self, message):
         self.message = message
+
+
+class MissingParamError(Exception):
+    def __init__(self, param_name):
+        super().__init__(f"Missing param: {param_name}")
