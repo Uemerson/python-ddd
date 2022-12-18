1384679ebc09e67af71be78ce5968e24d9247b44
Uemerson Pinheiro Junior <uemersonpinheirojunior@gmail.com>
2022-12-18 16:04:04 -0300

    refactor: customer view set

--- infrastructure/django/api/customers/views.py
+++ infrastructure/django/api/customers/views.py
@@ -1,27 +1,29 @@
+from django.utils import dateparse
 from rest_framework import viewsets
 from rest_framework.response import Response
-from django.utils import dateparse
 
 from booking_service.application.customers.customer_dto import CustomerDto
 from booking_service.application.customers.customer_manager import CustomerManager
+
 from .repositories import CustomerRepository
 
 
 class CustomerViewSet(viewsets.ViewSet):
+    __customer_manager = CustomerManager(CustomerRepository())
+
     def create(self, request):
         name, birth_date, document, email = [
             request.data.get(k, None)
             for k in ("name", "birth_date", "document", "email")
         ]
-        dto = CustomerDto(
-            name=name,
-            birth_date=dateparse.parse_datetime(birth_date),
-            document=document,
-            email=email,
+        http_response = self.__customer_manager.create_new_customer(
+            CustomerDto(
+                name=name,
+                birth_date=dateparse.parse_datetime(birth_date),
+                document=document,
+                email=email,
+            )
         )
-        customer_repository = CustomerRepository()
-        customer_manager = CustomerManager(customer_repository)
-        http_response = customer_manager.create_new_customer(dto)
         return Response(
             http_response["body"]
             if http_response["statusCode"] >= 200 and http_response["statusCode"] <= 299
